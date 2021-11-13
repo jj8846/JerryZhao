912047de325fcef613cdf053cdeaafbf660ad478
Jerry <9627724+jj8846@user.noreply.gitee.com>
2021-11-13 23:48:56 +0800

    游戏制作第一部分，包括了地图的绘制，角色的创建，角色创建包含他的物理模型，碰撞模型，移动函数的书写。

---
+++ FirstGame/Assets/Scripts/playercontrol.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    public float speed;
+    public float jumpforce;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+    }
+    private void Movement()
+    {
+        float Horizontalmove = Input.GetAxis("Horizontal");
+        float facedirection = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(Horizontalmove * speed, rb.velocity.y);
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(Horizontalmove * speed, jumpforce);
+        }
+        if(facedirection != 0)
+        {
+            transform.localScale = new Vector3(facedirection, 1, 1);
+        }
+    }
+}
